bce5707d52f59e8333d5f1a7e89975f2558de1a1
EstefaniaCarreon <202310054@itslerdo.edu.mx>
2024-05-07 17:23:48 -0600

    ASBC

--- app/Http/Controllers/RegistrarController.php
+++ app/Http/Controllers/RegistrarController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Estudiante;
+use App\Models\Tutor;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -25,6 +27,28 @@ class RegistrarController extends Controller
 
         Auth::login($user);
 
+        if (condition) {
+            $tutor = new Tutor();
+
+            $tutor->nombre = $request->nombre;
+            $tutor->apellido_paterno = $request->apellido_paterno;
+            $tutor->apellido_materno = $request->apellido_metrno;
+            $tutor->fecha_nacimiento = $request->fecha_nacimiento;
+            $tutor->sexo = $request->sexo;
+
+            $tutor->save();
+        }else {
+            $estudiante = new Estudiante();
+
+            $estudiante->nombre = $request->nombre;
+            $estudiante->apellido_paterno = $request->apellido_paterno;
+            $estudiante->apellido_materno = $request->apellido_metrno;
+            $estudiante->fecha_nacimiento = $request->fecha_nacimiento;
+            $estudiante->sexo = $request->sexo;
+
+            $estudiante->save();
+        }
+        
         return redirect(route('home'));
     }
 
