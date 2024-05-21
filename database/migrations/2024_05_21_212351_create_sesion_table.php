91d72897ad62942815fe2d4ae0fae1d7c7081042
ednasaahmed <202310068@itslerdo.edu.mx>
2024-05-21 15:50:54 -0600

    LACM

---
+++ database/migrations/2024_05_21_212351_create_sesion_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sesion', function (Blueprint $table) {
+            $table->integer('id_sesion')->autoIncrement();
+            $table->integer('id_tutor');
+            $table->foreign('id_tutor')->references('id_tutor')->on('tutores');
+            $table->integer('id_estudiante');
+            $table->foreign('id_estudiante')->references('id_estudiante')->on('estudiantes');
+            $table->date('fecha');
+            $table->integer('id_materia');
+            $table->foreign('id_materia')->references('id_materia')->on('materia');
+            $table->time('hora_inicio');
+            $table->time('hora_fin');
+            $table->binary('estado');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sesion');
+    }
+};
