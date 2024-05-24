62869c08d2206a295383791c3c9eebc86a1b46e1
ednasaahmed <202310068@itslerdo.edu.mx>
2024-05-24 14:03:16 -0600

    LACM

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -14,17 +14,20 @@ class CalendarController extends Controller
     {
        // $sesion=sesion::all();
         $user = Auth::user();
-        $estudiante = $user->estudiante; 
-        $sesiones = Sesion::with(['tutores', 'estudiantes', 'materia'])->get()->map(function ($sesion) {
-            return [
-                'id_sesion' => $sesion->id_sesion,
-                'fecha' => $sesion->fecha->format('Y-m-d\TH:i:s'),
-                'nombre_tutor' => $sesion->tutores ? $sesion->tutores->nombre : 'No especificado',
-                'nombre_estudiante' => $sesion->estudiantes ? $sesion->estudiantes->nombre : 'No especificado',
-                'nombre_materia' => $sesion->materia ? $sesion->materia->Nombre : 'No especificado',
-                'estado' => $sesion->estado,
-            ];
-        });
+        $estudiante  = $user->estudiante; 
+        $sesiones = Sesion::with(['tutores', 'estudiantes', 'materia'])
+        ->where('id_estudiante', $estudiante->id_estudiante)
+        ->get()
+        ->map(function ($sesion) {
+                return [
+                    'id_sesion' => $sesion->id_sesion,
+                    'fecha' => $sesion->fecha->format('Y-m-d\TH:i:s'),
+                    'nombre_tutor' => $sesion->tutores ? $sesion->tutores->nombre : 'No especificado',
+                    'nombre_estudiante' => $sesion->estudiantes ? $sesion->estudiantes->nombre : 'No especificado',
+                    'nombre_materia' => $sesion->materia ? $sesion->materia->Nombre : 'No especificado',
+                    'estado' => $sesion->estado,
+                ];
+            });
 
         return view('calendar', compact('user', 'estudiante','sesiones'));
     }
--- app/Http/Controllers/DetMateriaController.php
+++ app/Http/Controllers/DetMateriaController.php
@@ -41,16 +41,19 @@ class DetMateriaController extends Controller
                 $sesion->save();
                 $materia = Materia::findOrFail($id_materia);
                 $tutor = Tutores::findOrFail($id);
-                $sesiones = Sesion::with(['tutores', 'estudiantes', 'materia'])->get()->map(function ($sesion) {
-                    return [
-                        'id_sesion' => $sesion->id_sesion,
-                        'fecha' => $sesion->fecha->format('Y-m-d\TH:i:s'),
-                        'nombre_tutor' => $sesion->tutores ? $sesion->tutores->nombre : 'No especificado',
-                        'nombre_estudiante' => $sesion->estudiantes ? $sesion->estudiantes->nombre : 'No especificado',
-                        'nombre_materia' => $sesion->materia ? $sesion->materia->Nombre : 'No especificado',
-                        'estado' => $sesion->estado,
-                    ];
-                });
+                $sesiones = Sesion::with(['tutores', 'estudiantes', 'materia'])
+                ->where('id_estudiante', $estudiante->id_estudiante)
+                ->get()
+                ->map(function ($sesion) {
+                        return [
+                            'id_sesion' => $sesion->id_sesion,
+                            'fecha' => $sesion->fecha->format('Y-m-d\TH:i:s'),
+                            'nombre_tutor' => $sesion->tutores ? $sesion->tutores->nombre : 'No especificado',
+                            'nombre_estudiante' => $sesion->estudiantes ? $sesion->estudiantes->nombre : 'No especificado',
+                            'nombre_materia' => $sesion->materia ? $sesion->materia->Nombre : 'No especificado',
+                            'estado' => $sesion->estado,
+                        ];
+                    });
 
                 return view('calendar',compact('tutor','estudiante','materia','sesiones'));
 
